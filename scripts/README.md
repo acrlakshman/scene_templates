e5989707b4d018874c91e5e0e90c903e04ca7441
Lakshman Anumolu <anumolu@uwalumni.com>
2020-04-18 14:25:22 -0500

    install script for singularity
    Signed-off-by: Lakshman Anumolu <anumolu@uwalumni.com>

---
+++ scripts/README.md
@@ -0,0 +1,6 @@
+Scripts to install utilities
+
+# install_singularity.sh
+    * OS: Linux
+    * go
+    * singularity
---
+++ scripts/install_singularity.sh
@@ -0,0 +1,25 @@
+echo "--------------------------------------------"
+echo "Please make sure to install dependencies before using this script"
+echo "--------------------------------------------"
+
+echo "Installing go"
+
+export VERSION=1.14.2 OS=linux ARCH=amd64 && \
+	wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
+	sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
+	rm go$VERSION.$OS-$ARCH.tar.gz
+
+echo 'export PATH=/usr/local/go/bin:$PATH' >> ~/.zshrc
+export PATH=/usr/local/go/bin:$PATH
+
+echo "Installing singularity"
+
+export VERSION=3.5.3 && \
+	wget https://github.com/sylabs/singularity/releases/download/v$VERSION/singularity-$VERSION.tar.gz && \
+	tar -xzf singularity-$VERSION.tar.gz && \
+	cd singularity && \
+	./mconfig && \
+	make -C builddir && \
+	sudo make -C builddir install && \
+	cd .. && rm singularity-$VERSION.tar.gz && \
+	rm -r singularity && go version
