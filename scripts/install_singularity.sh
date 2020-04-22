152576ec51b7aeeac743b73a67422717edbce146
Lakshman Anumolu <anumolu@uwalumni.com>
2020-04-22 01:07:00 -0500

    minor cleanup
    Signed-off-by: Lakshman Anumolu <anumolu@uwalumni.com>

--- scripts/install_singularity.sh
+++ scripts/install_singularity.sh
@@ -1,10 +1,10 @@
 echo "Installing go"
 
 export VERSION=1.14.2 OS=linux ARCH=amd64 && \
-	wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
-	mkdir -p $HOME/.local && \
-	tar -C $HOME/.local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
-	rm go$VERSION.$OS-$ARCH.tar.gz
+    wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
+    mkdir -p $HOME/.local && \
+    tar -C $HOME/.local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
+    rm go$VERSION.$OS-$ARCH.tar.gz
 
 echo 'export PATH=$HOME/.local/go/bin:$PATH' >> ~/.bashrc
 export PATH=$HOME/.local/go/bin:$PATH
@@ -25,10 +25,9 @@ sudo apt-get update && sudo apt-get install -y \
 
 export VERSION=3.5.3 && \
 	wget https://github.com/sylabs/singularity/releases/download/v$VERSION/singularity-$VERSION.tar.gz && \
-	tar -xzf singularity-$VERSION.tar.gz && \
+	tar -xzf singularity-$VERSION.tar.gz && rm singularity-$VERSION.tar.gz && \
 	cd singularity && \
 	./mconfig && \
 	make -C builddir && \
 	sudo make -C builddir install && \
-	cd .. && rm singularity-$VERSION.tar.gz && \
-	rm -r singularity
+	cd .. && rm -r singularity
